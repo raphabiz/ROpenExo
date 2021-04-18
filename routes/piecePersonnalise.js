66120654efee726ef3fce97de90eefdb3ff33984
abazabas <alexisbazabas@gmail.com>
2021-04-18 22:47:37 +0200

    Delete piecePersonnalise.js

--- routes/piecePersonnalise.js
+++
@@ -1,5 +0,0 @@
-const express =  require('express');
-const app = express();
-require('./models/dbConfig');
-const postsRoutes = require('./routes/pieceController');
-const bodyParser = require('body-parser');
